e408bc3afe3d3f123f0970a1f0dd79c7d6601f0e
Wilson Snyder <wsnyder@wsnyder.org>
2009-01-05 15:38:39 +0000

    Copyright and year update
    git-svn-id: file://localhost/svn/Parallel-Forker/trunk/Parallel-Forker@67196 c99bdd1b-02e9-0310-b855-c9df8d5cb90a

--- lib/Parallel/Forker.pm
+++ lib/Parallel/Forker.pm
@@ -1,18 +1,4 @@
-# Fork.pm -- Parallel management
-# $Id$
-######################################################################
-#
-# This program is Copyright 2002-2008 by Wilson Snyder.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of either the GNU General Public License or the
-# Perl Artistic License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
+# See copyright, etc in below POD section.
 ######################################################################
 
 package Parallel::Forker;
@@ -526,7 +512,7 @@ Print a dump of the execution tree.
 The latest version is available from CPAN and from
 L<http://www.veripool.org/>.
 
-Copyright 2002-2008 by Wilson Snyder.  This package is free software; you
+Copyright 2002-2009 by Wilson Snyder.  This package is free software; you
 can redistribute it and/or modify it under the terms of either the GNU
 Lesser General Public License or the Perl Artistic License.
 
