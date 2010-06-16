0f29dd610d61a421ddb2ac0cc11df40cc9881027
Wilson Snyder <wsnyder@wsnyder.org>
2010-06-16 12:03:44 -0400

    Version bump

--- lib/Parallel/Forker.pm
+++ lib/Parallel/Forker.pm
@@ -11,7 +11,7 @@ use Parallel::Forker::Process;
 use strict;
 use vars qw($Debug $VERSION);
 
-$VERSION = '1.225';
+$VERSION = '1.230';
 
 ######################################################################
 #### CONSTRUCTOR
